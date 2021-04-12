c629c4861ca602878fc5a9ea55940700a1f395d6
“Preston <prwi6365@colorado.edu>
2021-04-12 15:31:21 -0600

    added factorial function

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -2,7 +2,15 @@
 
 using namespace std;
 
+int factorial(int num) {
+    if (num == 0) return 1;
+    else return num * factorial(num - 1);
+}
+
 int main() {
     cout << "hello world" << endl;
+    
+    cout << factorial(5) << endl;
+    
     return 0;
 }
